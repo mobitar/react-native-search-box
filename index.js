36e34e7ddebc9b65ab1fc67c2905d145199db91c
Antonella Sgarlatta <antsgar@gmail.com>
2021-03-17 15:45:38 -0300

    fix: fix parallel animations

--- index.js
+++ index.js
@@ -185,33 +185,33 @@ class Search extends PureComponent {
         toValue: this.contentWidth - this.cancelButtonWidth,
         duration: 200,
         useNativeDriver: true,
-      }).start(),
+      }),
       Animated.timing(this.btnCancelAnimated, {
         toValue: 10,
         duration: 200,
         useNativeDriver: false,
-      }).start(),
+      }),
       Animated.timing(this.inputFocusPlaceholderAnimated, {
         toValue: this.props.placeholderExpandedMargin,
         duration: 200,
         useNativeDriver: false,
-      }).start(),
+      }),
       Animated.timing(this.iconSearchAnimated, {
         toValue: this.props.searchIconExpandedMargin,
         duration: 200,
         useNativeDriver: false,
-      }).start(),
+      }),
       Animated.timing(this.iconDeleteAnimated, {
         toValue: this.state.keyword.length > 0 ? 1 : 0,
         duration: 200,
         useNativeDriver: true,
-      }).start(),
+      }),
       Animated.timing(this.shadowOpacityAnimated, {
         toValue: this.props.shadowOpacityExpanded,
         duration: 200,
         useNativeDriver: false,
-      }).start()
-    ]);
+      })
+    ]).start();
     this.shadowHeight = this.props.shadowOffsetHeightExpanded;
   };
 
@@ -222,37 +222,37 @@ class Search extends PureComponent {
         toValue: this.contentWidth - 10,
         duration: 200,
         useNativeDriver: false,
-      }).start(),
+      }),
       Animated.timing(this.btnCancelAnimated, {
         toValue: this.contentWidth,
         duration: 200,
         useNativeDriver: false,
-      }).start(),
+      }),
       this.props.keyboardShouldPersist === false
         ? Animated.timing(this.inputFocusPlaceholderAnimated, {
             toValue: this.middleWidth - this.props.placeholderCollapsedMargin,
             duration: 200,
             useNativeDriver: false,
-          }).start()
+          })
         : null,
       this.props.keyboardShouldPersist === false || isForceAnim === true
         ? Animated.timing(this.iconSearchAnimated, {
             toValue: this.middleWidth - this.props.searchIconCollapsedMargin,
             duration: 200,
             useNativeDriver: false,
-          }).start()
+          })
         : null,
       Animated.timing(this.iconDeleteAnimated, {
         toValue: 0,
         duration: 200,
         useNativeDriver: true,
-      }).start(),
+      }),
       Animated.timing(this.shadowOpacityAnimated, {
         toValue: this.props.shadowOpacityCollapsed,
         duration: 200,
         useNativeDriver: true,
-      }).start()
-    ]);
+      })
+    ]).start();
     this.shadowHeight = this.props.shadowOffsetHeightCollapsed;
   };
 
